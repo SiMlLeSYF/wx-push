e2b0a4e265ada82e0728cb8fd54e2ecec9b223ca
SYF <48353885+SiMlLeSYF@users.noreply.github.com>
2023-04-19 14:41:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx3613e378ace336c4",
+  appsecret: "b1cb51a23282f3bb9c14e41df3b536ed",
+  template_id: "qSHBw7mh2eXsS3cdqsIT7IUWyOKRD_yHBk_UsITdc0qk",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "青岛",
   first_date: "2022-11-10",
 };
